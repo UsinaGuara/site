eda3dab996ab8beb2411b1a4a34721b7c295344a
Laysa <laysabernardes.ads@gmail.com>
2025-12-19 19:41:58 -0300

    chore: desativando travas do TS e ajustando build

--- frontend/src/ProtectedRoute.tsx
+++ frontend/src/ProtectedRoute.tsx
@@ -1,6 +1,10 @@
 import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children }: ProtectedRouteProps)=> {
   const token = true; // aqui vai precisar de cookies
   if (!token) {
     return <Navigate to="/login" />;
